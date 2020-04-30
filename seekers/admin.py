eac4d6cca97e14cf68e592f35114c013a4dc9788
Vicki <vicki.langer@statalog.org>
2020-04-30 10:06:08 -0400

    add columns and column names

--- seekers/admin.py
+++ seekers/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Listing
+
 
 # Register your models here.
+class ListingAdmin(admin.ModelAdmin):
+    # a list of displayed columns name.
+    list_display = ['id', 'seeker', 'job_title', 'location', 'skills', 'links', 'availability']
+    search_fields = ['job_title', 'location']
+    list_filter = ['job_title', 'location', 'availability']
+
+
+admin.site.register(Listing, ListingAdmin)
